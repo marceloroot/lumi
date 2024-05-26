f20c0715467da6bcff664078073b48c9523bc427
marceloroot <marcelo.lima.gomes.23@gmail.com>
2024-05-26 14:08:58 -0300

    fix: init sql create if exist

--- init-postgres.sql
+++ init-postgres.sql
@@ -1,1 +1,11 @@
-CREATE DATABASE db_lumi;
+-- init-postgres.sql
+CREATE OR REPLACE FUNCTION create_database_if_not_exists(db_name TEXT) RETURNS VOID AS $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = db_name) THEN
+        CREATE DATABASE db_name;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Chamada da função para criar o banco de dados db_lumi se ele não existir
+SELECT create_database_if_not_exists('db_lumi');
